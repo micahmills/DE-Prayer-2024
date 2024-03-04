21258b1705de3acb23ff2759c8cda859cfb6770a
corsacca <corsacca@gmail.com>
2024-03-04 21:25:53 +0100

    Enable Arabic

--- admin/admin-menu-and-tabs.php
+++ admin/admin-menu-and-tabs.php
@@ -110,7 +110,7 @@ class Ramadan_2024_Tab_General {
         }
 
 
-        $prayer_fuel_ready = [ 'en_US', 'pt_BR', 'fr_FR' ];
+        $prayer_fuel_ready = [ 'en_US', 'pt_BR', 'fr_FR', 'ar' ];
 
         ?>
         <table class="widefat striped">
